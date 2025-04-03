333d0632677cf7b9815e3cc53bdd5ebe61a4be97
Fernando Antunes Filho <fernando.antunes1@gmail.com>
2025-04-03 18:59:39 -0300

    cria list e form para especialidades

---
+++ src/pages/status/StatusFormPage.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+import { DefaultTemplate } from '@/template'
+</script>
+
+<template>
+  <default-template><h1>Inserir Status</h1></default-template>
+</template>
--- src/router/routes.ts
+++ src/router/routes.ts
@@ -4,5 +4,27 @@ export const routes = [
     name: 'dashboard',
     component: () =>
       import(/* webpackChunkName: "dashboard" */ '@/pages/dashboard/DashboardPage.vue')
+  },
+  {
+    path: '/status',
+    name: 'status-list',
+    component: () => import(/* webpackChunkName: "status" */ '@/pages/status/StatusListPage.vue')
+  },
+  {
+    path: '/status/insert',
+    name: 'status-insert',
+    component: () => import(/* webpackChunkName: "status" */ '@/pages/status/StatusFormPage.vue')
+  },
+  {
+    path: '/specialty',
+    name: 'specialty-list',
+    component: () =>
+      import(/* webpackChunkName: "specialty" */ '@/pages/specialty/SpecialtyListPage.vue')
+  },
+  {
+    path: '/specialty/insert',
+    name: 'specialty-insert',
+    component: () =>
+      import(/* webpackChunkName: "status" */ '@/pages/specialty/SpecialtyFormPage.vue')
   }
 ]
